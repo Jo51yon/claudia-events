dff5c36a6f96e7489d070b292d0e6e0acfd49ed2
Jo51yon <jolyon.grace@gracenet.co.uk>
2026-08-24 22:42:33 +0100

    initial: src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as ClaudiaEventDetail } from './ClaudiaEventDetail';
+export type { ClaudiaEventDetailProps, ClaudiaEventDetailCopy } from './ClaudiaEventDetail';
+export { default as ClaudiaEventList } from './ClaudiaEventList';
+export type { ClaudiaEventListProps, ClaudiaEventListCopy } from './ClaudiaEventList';
+export type { ClaudiaEvent, ClaudiaEventRsvp, ClaudiaRsvpStatus } from './types';
